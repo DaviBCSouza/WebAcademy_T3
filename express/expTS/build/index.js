7c8db95477d8ff68ab754ea0c12dcfb7165b5700
DaviBCSouza <contato@davibrenno.com>
2024-04-02 12:38:18 -0400

    Aula 02 - Express: Exercício 05

--- express/expTS/build/index.js
+++ express/expTS/build/index.js
@@ -5,13 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const loggers_1 = __importDefault(require("./middlewares/loggers"));
 const validateEnv_1 = __importDefault(require("./utils/validateEnv"));
 dotenv_1.default.config();
 (0, validateEnv_1.default)();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT ?? 5000;
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+app.use((0, loggers_1.default)('completo'));
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 app.listen(PORT, () => {
     console.log(`Aplicação rodando em http://localhost:${PORT}`);
