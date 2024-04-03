b0f09990e4f58c3ca95bfbe5cc8c332727b9e6da
DaviBCSouza <contato@davibrenno.com>
2024-04-03 11:37:06 -0400

    Aula 03 - Express: Exercício 07

--- express/expTS/src/index.ts
+++ express/expTS/src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { engine } from 'express-handlebars';
 import logger from './middlewares/loggers';
 import router from './router/router';
 import validateEnv from './utils/validateEnv';
@@ -11,6 +12,10 @@ const app = express();
 const PORT = process.env.PORT ?? 5000;
 
 app.use(logger('completo'));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/views`);
 
 app.use(router);
 
--- express/expTS/src/router/router.ts
+++ express/expTS/src/router/router.ts
@@ -17,6 +17,35 @@ router.get('/lorem/:paragraphs', (req: Request, res: Response) => {
   res.send(loremText);
 });
 
+router.get('/hb1', (req: Request, res: Response) => {
+  res.render('hb1', {
+    mensagem: 'Hello World with the Handlebars!',
+    layout: false
+  });
+});
+
+router.get('/hb2', (req: Request, res: Response) => {
+  res.render('hb2', {
+    poweredByExpress: true,
+    name: 'Express',
+    type: 'Framework',
+    layout: false
+  });
+});
+
+router.get('/hb3', (req: Request, res: Response) => {
+  res.render('hb3', {
+    setor: 'Professores do ICOMP',
+    professores: [
+      { nome: 'David Fernandes', sala: 1238 },
+      { nome: 'Horácio Fernandes', sala: 1233 },
+      { nome: 'Edleno Moura', sala: 1236 },
+      { nome: 'Elaine Harada', sala: 1231 }
+    ],
+    layout: false
+  });
+});
+
 router.use((req: Request, res: Response) => {
   res.status(404).send('Error 404!');
 });
