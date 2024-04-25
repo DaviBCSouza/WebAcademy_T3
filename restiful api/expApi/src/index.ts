b63ab5a7680471a27bf8489a75afcd6a0300636c
DaviBCSouza <contato@davibrenno.com>
2024-04-24 21:31:29 -0400

    Aula 02 - RESTful API: Exercício 03

--- restiful api/expApi/src/index.ts
+++ restiful api/expApi/src/index.ts
@@ -1,6 +1,8 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 
+import setCookieLang from "./middlewares/setCookieLang";
 import router from "./router";
 import validateEnv from "./utils/validateEnv";
 
@@ -10,6 +12,8 @@ validateEnv();
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
+app.use(cookieParser());
+app.use(setCookieLang);
 app.use(express.json());
 app.use(router);
 
